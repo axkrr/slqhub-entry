b9fee98125f765fc6f1e7baf3f02b3d16dc79494
axkr <135993682+axkrr@users.noreply.github.com>
2026-01-08 22:36:26 +0800

    更新 entry.js

--- entry.js
+++ entry.js
@@ -1,7 +1,7 @@
 // QX 公有入口，仅负责拉私有逻辑
 
 const CORE_URL =
-  "https://raw.githubusercontent.com/你的用户名/你的私有仓库/main/core.js";
+  "https://raw.githubusercontent.com/axkrr/slqhub/main/QuantumultX/Rewrite/JS/core.js";
 
 $httpClient.get(CORE_URL, (err, resp, data) => {
   if (err) {
