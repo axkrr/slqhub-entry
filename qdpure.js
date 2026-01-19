fa128d2fde45f35a86e3293db885d3ea6ca18867
axkr <135993682+axkrr@users.noreply.github.com>
2026-01-19 11:07:16 +0800

    更新 qdpure.js

--- qdpure.js
+++ qdpure.js
@@ -7,21 +7,11 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-console.log("qdpure-original: " + body);
-
+// 修改数据
 obj.load = 0;
 obj.bid = [];
+obj.data = [];
 obj.lns = 0;
-
-if (obj.data && Array.isArray(obj.data)) {
-    obj.data = obj.data.map(item => {
-        item.id = ""; 
-        item.value = "";
-        return item;
-    });
-}
-
-console.log("qdpure-modified: " + JSON.stringify(obj));
 
 // 重新打包返回
 body = JSON.stringify(obj);
