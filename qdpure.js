5eb338b8c79d86f78ef93c1b34e5596226672977
axkr <135993682+axkrr@users.noreply.github.com>
2026-01-19 10:46:11 +0800

    更新 qdpure.js

--- qdpure.js
+++ qdpure.js
@@ -7,11 +7,18 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-// 修改数据
-obj.load = 0;
-obj.bid = [];
+// 核心配置清空
 obj.data = [];
+obj.bid = [];
+
+// 针对广告位逻辑的深度净化
+obj.load = 0;
 obj.lns = 0;
+obj.code = 0; // 有些Appcode不为1就不渲染布局
+
+// 如果响应中存在其他潜在的广告配置字段,一并处理
+if (obj.extra) obj.extra = {};
+if (obj.config) obj.config = {};
 
 // 重新打包返回
 body = JSON.stringify(obj);
