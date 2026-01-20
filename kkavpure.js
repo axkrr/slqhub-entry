09191fabaabaf61c555d05db35838a7525e60fcd
axkr <135993682+axkrr@users.noreply.github.com>
2026-01-21 01:16:16 +0800

    更新 kkavpure.js

--- kkavpure.js
+++ kkavpure.js
@@ -1,6 +1,6 @@
 /**
  * @name kkavpure
- * @desc 屏蔽弹窗和轮播广告接口
+ * @desc kkav广告净化
  * @author axkrr
 */
 
@@ -10,7 +10,7 @@ try {
     let obj = JSON.parse(body);
     // 强制关闭显示开关
     obj.show = 0;
-    // 置空加密数据，防止本地解密逻辑继续执行
+    // 置空加密数据,防止本地解密逻辑继续执行
     obj.data = {
         "key": "",
         "data": ""
@@ -20,6 +20,6 @@ try {
     
     $done({ body: JSON.stringify(obj) });
 } catch (e) {
-    // 发生异常时直接原样返回，不影响APP正常加载
+    // 发生异常时直接原样返回,不影响APP正常加载
     $done({ body });
 }
