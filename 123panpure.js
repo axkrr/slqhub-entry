367bb7d16574123e854e751cf5d8102a2aeaf809
axkr <135993682+axkrr@users.noreply.github.com>
2026-01-21 16:15:41 +0800

    更新 123panpure.js

--- 123panpure.js
+++ 123panpure.js
@@ -5,28 +5,40 @@
 */
 
 const url = $request.url;
-let obj = JSON.parse($response.body || '{}');
+let body = $response.body;
 
-// 处理TopOn广告位配置请求
-if (url.indexOf('open/placement') !== -1) {
-    obj.data = {};
-    obj.msg = "Success";
-    obj.code = 0;
+try {
+    let obj = JSON.parse(body);
+
+    // 处理Placement配置
+    if (url.indexOf('open/placement') !== -1) {
+        obj.data = {};
+        obj.msg = "Success";
+        obj.code = 0;
+    } 
+    // 处理Bid竞价
+    else if (url.indexOf('/bid') !== -1) {
+        obj.data = [];
+        obj.msg = "success";
+        obj.code = 0;
+    } 
+    // 处理最新的Request广告内容请求
+    else if (url.indexOf('/request') !== -1) {
+        if (obj.data) {
+            obj.data.seatbid = []; // 清空广告位列表
+        }
+        obj.msg = "success";
+        obj.code = 0;
+    }
+    // 处理领野广告Map
+    else if (url.indexOf('ad/map') !== -1) {
+        obj.data = [];
+        obj.bid = [];
+        obj.code = 1;
+        obj.message = "成功";
+    }
+
+    $done({ body: JSON.stringify(obj) });
+} catch (e) {
+    $done({ body });
 }
-
-// 处理TopOn实时竞价请求
-if (url.indexOf('/bid') !== -1) {
-    obj.data = [];
-    obj.msg = "success";
-    obj.code = 0;
-}
-
-// 处理领野广告Map请求
-if (url.indexOf('ad/map') !== -1) {
-    obj.data = [];
-    obj.bid = [];
-    obj.code = 1;
-    obj.message = "成功";
-}
-
-$done({ body: JSON.stringify(obj) });
